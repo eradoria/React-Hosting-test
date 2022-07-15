a812b94793a8f94976b741c3564235e31f87b9c3
eradoria <eradoria@gmail.com>
2022-07-14 20:33:55 -0500

    1

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+export function Button(props) {
+  const { label = "Default", handleClick = () => console.log("I am Default") } =
+    props;
+  return <button onClick={handleClick}>{label}</button>;
+}
+
+
+// export class Button extends React.Component{
+//   constructor(props){
+//     super(props)
+//   }
+// };
+
+// render() {
+//   return <button onClick={this.props.handleClick}>{this.props.label}</button>
+// }
